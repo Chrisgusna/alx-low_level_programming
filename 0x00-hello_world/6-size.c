f00be919341749f25472a2b938d38a5bda118bc8
Chrisgusna <christopherlegacy01@gmail.com>
2023-02-16 15:21:06 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function for task 6
+ *
+ * Return 0 on success on the code
+ */
+
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s0", sizeof(long long int));
+	printf("size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
